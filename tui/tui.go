412518bff5e9ffa10e250f472b57ce592e01d286
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-05-17 13:22:59 +0200

    feat: hello world program

--- tui/tui.go
+++ tui/tui.go
@@ -1,73 +1,36 @@
 package tui
 
 import (
-	"fmt"
-	"os"
-
 	tea "charm.land/bubbletea/v2"
 )
 
-type model struct {
-	choices []string
-	cursor  int
-	chosen  string
+type Model struct {
 }
 
-func initialModel() model {
-	return model{
-		choices: []string{"playlists", "another option"},
-	}
-}
-
-func (m model) Init() tea.Cmd {
+func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
+	case tea.KeyPressMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-		case "enter":
-			m.chosen = m.choices[m.cursor]
-			return m, tea.Quit
 		}
 	}
-
 	return m, nil
 }
 
-func (m model) View() tea.View {
-	s := "Where would you like to go?\n\n"
-	for i, choice := range m.choices {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
-		s += fmt.Sprintf("%s %s\n", cursor, choice)
+func (m Model) View() tea.View {
+	return tea.View{
+		Content:   "Hello, World!\n",
+		AltScreen: true,
 	}
-
-	s += "\nPress enter to select. Press q to quit.\n"
-	return tea.NewView(s)
 }
 
-func RunTUI() {
-	p := tea.NewProgram(initialModel())
-	m, err := p.Run()
-	if err != nil {
-		fmt.Printf("There's been an error: %v", err)
-		os.Exit(1)
-	}
-	if m, ok := m.(model); ok && m.chosen != "" {
-		fmt.Println(m.chosen)
-	}
+func RunTUI() error {
+	p := tea.NewProgram(Model{})
+	_, err := p.Run()
+	return err
 }
