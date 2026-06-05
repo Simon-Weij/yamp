5191a005c1ea6453d6280c4461b483ca2cfb55e5
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-06-05 22:25:25 +0200

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:Simon-Weij/nixpkgs/wails3";
+    flakelight.url = "github:nix-community/flakelight";
+  };
+  outputs = {flakelight, ...} @ inputs:
+    flakelight ./. {
+      inherit inputs;
+      devShell.packages = pkgs: with pkgs; [wails3 go pnpm nodejs-slim pkg-config webkitgtk_6_0];
+    };
+}
