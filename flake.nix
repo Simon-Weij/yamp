918092c1fa5e59c01110da94efc3c189dfa0be67
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-06-29 23:36:50 +0200

    refactor: added wails3 as callPackage and got rid of flakelight

--- flake.nix
+++ flake.nix
@@ -1,14 +1,25 @@
 {
   inputs = {
-    nixpkgs.url = "github:Simon-Weij/nixpkgs/wails3";
-    flakelight.url = "github:nix-community/flakelight";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = {flakelight, ...} @ inputs:
-    flakelight ./. {
-      inherit inputs;
-      devShell = pkgs: {
-        packages = with pkgs; [wails3 go pnpm nodejs-slim pkg-config webkitgtk_6_0 golangci-lint just glib-networking];
+
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = let
+          wails3 = pkgs.callPackage ./nix/wails3.nix {};
+        in
+          with pkgs; [wails3 go pnpm nodejs-slim pkg-config webkitgtk_6_0 golangci-lint just glib-networking clang clang-tools];
+
         env.GIO_EXTRA_MODULES = "${pkgs.glib-networking}/lib/gio/modules";
+
+        buildInputs = with pkgs; [
+          mpv
+        ];
       };
-    };
+    });
+  };
 }
